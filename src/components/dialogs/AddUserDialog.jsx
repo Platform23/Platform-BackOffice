248e50f6a9918198d2fffc00b3f758f1c393fdae
Platform23 <platform.kolonn@gmail.com>
2024-08-10 17:57:00 -0400

    fixing adding user component

--- src/components/dialogs/AddUserDialog.jsx
+++ src/components/dialogs/AddUserDialog.jsx
@@ -7,16 +7,31 @@ import {
   TextField,
   Button,
   Grid,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  IconButton
 } from '@mui/material';
+import {Visibility, VisibilityOff} from '@mui/icons-material';
+import { communityProfile, communities, competences } from '../../utils/Constant';
 
 
 const AddUserDialog = ({ open, onClose, onAdd}) => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
     const [formData, setFormData] = React.useState({
-        username: '',
+        pseudo: '',
         email: '',
-        profession: '',
-        skills: '',
-        networks: '',
+        password: '',
+        competences: [],
+        communities: [],
+        profiles: [],
     });
 
     const handleChange = (e) => {
@@ -24,7 +39,7 @@ const AddUserDialog = ({ open, onClose, onAdd}) => {
             ...formData,
             [e.target.name]: e.target.value,
         });
-        console.log("User added successfully");
+        // console.log("User added successfully");
     };
 
   return (
@@ -61,36 +76,98 @@ const AddUserDialog = ({ open, onClose, onAdd}) => {
                         variant="outlined"
                     />
                 </Grid>
-                <Grid item xs={6}>
-                    <TextField
-                        label="Profession"
-                        name="profession"
-                        value={formData.skills}
-                        onChange={handleChange}
-                        fullWidth
-                        variant="outlined"
-                    />
-                </Grid>
-                <Grid item xs={6}>
-                    <TextField
-                        label="Competences"
-                        name="competences"
-                        value={formData.skills}
-                        onChange={handleChange}
-                        fullWidth
-                        variant="outlined"
-                    />
-                </Grid>
                 <Grid item xs={12}>
-                    <TextField
-                        label="Reseaux"
-                        name="reseaux"
-                        value={formData.networks}
-                        onChange={handleChange}
-                        fullWidth
-                        variant="outlined"
-                    />
+                    <FormControl sx={{ mt: 3}} variant="filled" fullWidth>
+                        <InputLabel htmlFor="outlined-adornment-password" variant="outlined" fullWidth>Mot de passe</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            name="password"
+                            type={showPassword ? 'text' : 'password'}
+                            value={formData.password}
+                            onChange={handleChange}
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                                >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
                 </Grid>
+
+                <Grid item xs={12}>
+                    <FormControl fullWidth>
+                        <InputLabel id="profiles-label">Profil et rôle</InputLabel>
+                        <Select
+                            labelId="profiles-label"
+                            id="profiles"
+                            name="profiles"
+                            value={formData.profiles}
+                            onChange={handleChange}
+                            label="Profil et rôle"
+                            multiple
+                        >
+                            {/* <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem> */}
+                            {communityProfile.map((profile) => (
+                                <MenuItem key={profile.value} value={profile.value}>
+                                    {profile.label}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <FormControl fullWidth>
+                        <InputLabel id="competences-label">Compétences</InputLabel>
+                        <Select
+                            labelId="competences-label"
+                            id="competences"
+                            name="competences"
+                            value={formData.competences}
+                            onChange={handleChange}
+                            label="Compétences"
+                            multiple
+                        >
+                            {competences.map((comp) => (
+                                <MenuItem key={comp.value} value={comp.value}>
+                                    {comp.label}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <FormControl fullWidth>
+                        <InputLabel id="communities-label">Communauté d'appartenance</InputLabel>
+                        <Select
+                            labelId="communities-label"
+                            id="communities"
+                            name="communities"
+                            value={formData.communities}
+                            onChange={handleChange}
+                            label="Communauté d'appartenance"
+                            multiple
+                        >
+                            {communities.map((communitie) => (
+                                <MenuItem key={communitie.value} value={communitie.value}>
+                                    {communitie.label}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+
             </Grid>
         </DialogContent>
 
---
+++ src/utils/Constant.js
@@ -0,0 +1,43 @@
+export const communityProfile = [
+    { value: 1, label: "Utilisateur / Utilisatrice (Étudiant.e ; Chercheur.e…)", },
+    { value: 2, label: "Volontaire / Bénévole", },
+    { value: 3, label: "Réparateur.trice", },
+    { value: 4, label: "Fab-manager", },
+    { value: 5, label: "Community manager", },
+    { value: 6, label: "Volontaire / Bénévole", },
+    { value: 7, label: "Logisticien.ne", },
+    { value: 8, label: "Développeur.euse d'interfaces", },
+    { value: 9, label: "Designer/ Programmeur.euse" },
+    { value: 10, label: "Concepteur.trice de produits technologiques", },
+];
+
+export const communities = [
+    { value: 1, label: "Fablabs", },
+    { value: 2, label: "Makerspaces", },
+    { value: 3, label: "Réparateurs", },
+    { value: 4, label: "Hackerspaces", },
+    { value: 5, label: "Living labs", },
+    { value: 6, label: "Communautés open source hardware", },
+    { value: 7, label: "Écoles de hackers et d'ateliers techniques", },
+    { value: 8, label: "Bibliothèque makerspaces", },
+    { value: 9, label: "Centre de formation professionnelle", },
+]
+
+export const competences = [
+    { value: 1, label: "Gestion de l'information et des médias numériques", },
+    { value: 2, label: "Communication et collaboration numériques", },
+    { value: 3, label: "Pensée computationnelle et critique", },
+    { value: 4, label: "Insertion et développement professionnel numérique", },
+    { value: 5, label: "Innovation et création numériques", },
+    { value: 6, label: "Gestion et pilotage de projets collaboratifs", },
+    { value: 7, label: "Accompagnement et formation", },
+]
+
+export const networks = [
+    { name: "Fablabs", description: "Ateliers de fabrication équipés de machines-outils à commande numérique", },
+    { name: "Hackerspaces", description: "Atelier collaboratif dédié aux projets utilisant l'électronique, la programmation, l'impression 3D etc.", },
+    { name: "Communautés d'artisans", description: "Communauté d’artisan.e.s et d’artistes qui partagent des espaces de travail et des ressources pour la création artistique.", },
+    { name: "Informatique", description: "Réseaux d'ateliers citoyens d'accès à la fabrication numérique et à l'accompagnement sur les nouvelles technologies.", },
+    { name: "Makerspaces", description: "Atelier collaboratif dédié au prototypage, à la fabrication numérique et à des projets collaboratifs entre particuliers et entrepreneurs.", },
+    { name: "Technique et Professionnelle", description: "Centre de formation technique et professionnelle.", },
+]
